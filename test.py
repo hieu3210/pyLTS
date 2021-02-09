c643c4e6e3deac5681be33335129dfcc3567d784
Nguyen Duy Hieu <hieu3210@gmail.com>
2021-02-09 13:21:26 +0700

    Complete HA model

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
-from HAs.HA import HA
+# This file for test only
+from HAs.HA import HedgeAlgebras
 
-p = HA(0.52, 0.49)
+theta = 0.55
+alpha = 0.52
 
-X = ["V-", "-", "L-", "W", "L+", "+", "V+"]
+p = HedgeAlgebras(theta, alpha)
 
+# X = ["V+", "W", "L+", "-", "L-", "+", "V-"]
+# print(X)
+# p.sort_terms(X)
+# print(X)
+
+X = p.get_words(3)
+print(len(X))
+print(X)
 for x in X:
     print(p.sqm(x))
