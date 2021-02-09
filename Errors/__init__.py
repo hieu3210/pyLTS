04e9aa1acdb2be19ab60de24daa6c998787bacbf
Nguyen Duy Hieu <hieu3210@gmail.com>
2021-02-09 18:17:09 +0700

    Developing

--- Errors/__init__.py
+++ Errors/__init__.py
@@ -1,8 +1,27 @@
 # Forecasting errors assessment measurements
+import math
 
 class Measure:
-    # Constructor
-    def __init__(self):
-        pass
+    # Constructor with actual historical data
+    def __init__(self, actual, forecasted):
+        self.__actual = actual
+        self.__forecasted = forecasted
+        self.__length = len(actual)
 
-    def MSE(self, actual, forecasted):
+    # Mean Squared Error
+    def mse(self):
+        err = 0
+        for i in range(self.__length):
+            err += math.pow(self.__actual[i] - self.__forecasted[i], 2)
+        return err / self.__length
+
+    # Root Mean Squared Error
+    def rmse(self):
+        return math.sqrt(self.mse())
+
+    # MAPE (%)
+    def mape(self):
+        err = 0
+        for i in range(self.__length):
+            err += abs((self.__actual[i] - self.__forecasted[i]) / self.__actual[i])
+        return err / self.__length * 100
--- test.py
+++ test.py
@@ -1,18 +1,28 @@
 # This file for test only
+from Errors import Measure
 from HAs import HedgeAlgebras
 
-theta = 0.55
-alpha = 0.52
+# Test error measurements
+a = [13563, 13867, 14696, 15460, 15311, 15603, 15861, 16807, 16919, 16388, 15433, 15497, 15145, 15163, 15984, 16859, 18150, 18970, 19328, 19337, 18876]
+b = [14537, 14537, 14537, 15534, 15534, 15534, 16019, 16019, 17162, 17162, 16019, 15534, 15534, 15534, 15514, 16019, 17162, 19217, 19217, 19217, 19217]
+m = Measure(a, b)
+c = m.rmse()
+print(c)
 
-p = HedgeAlgebras(theta, alpha)
-
-# X = ["V+", "W", "L+", "-", "L-", "+", "V-"]
+# Test HA Model
+# theta = 0.55
+# alpha = 0.52
+#
+# p = HedgeAlgebras(theta, alpha)
+#
+# # X = ["V+", "W", "L+", "-", "L-", "+", "V-"]
+# # print(X)
+# # p.sort_terms(X)
+# # print(X)
+#
+# X = p.get_words(0)
+# print(len(X))
 # print(X)
-# p.sort_terms(X)
-# print(X)
+# for x in X:
+#     print(p.sqm(x))
 
-X = p.get_words(3)
-print(len(X))
-print(X)
-for x in X:
-    print(p.sqm(x))
