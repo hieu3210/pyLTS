cd98853947437dc53f2ba95f0e52677e2e1dd766
Nguyen Duy Hieu <hieu3210@gmail.com>
2021-02-18 22:20:26 +0700

    Complete forecasting model (multi-order)

--- LTS/__init__.py
+++ LTS/__init__.py
@@ -1,15 +1,20 @@
 # Linguistic Time Series forecasting model
 from HAs import HedgeAlgebras
+import numpy as np
 
 
 class LTS:
     # Constructor
-    def __init__(self, data, lb, ub, words, theta, alpha):
+    def __init__(self, order, repeat, data, lb, ub, words, theta, alpha):
+        self.order = order  # The nth-order of LTS
+        self.repeat = repeat  # Weighted or no-weighted with repeat LLRs
         self.data = data  # Dataset list []
         self.lb = lb  # Lower bound of data
         self.ub = ub  # Upper bound of data
         self.words = words  # Words of HA model using in LTS model
         self.ha = HedgeAlgebras(theta, alpha)  # Instance of HA model to calculate the SQMs of words
+        self.lhs = []  # Left hand side list of rules
+        self.rhs = []  # Right hand side list of rules
 
     # Get semantics of words in [0,1]
     def get_semantic(self):
@@ -28,33 +33,69 @@ class LTS:
     # Get real semantics of historical data by words in HA
     def get_semantic_of_data(self):
         sem = []
+        real_sem = self.get_real_semantics()
         for d in self.data:
             min_distance = float("inf")
             min_distance_pos = 0
-            for x in self.get_real_semantics():
-                if abs(d - x) > min_distance:
+            for x in real_sem:
+                if abs(d - x) < min_distance:
                     min_distance = abs(d - x)
-                    min_distance_pos = self.words.index(x)
-            sem.append(self.get_real_semantics()[min_distance_pos])
+                    min_distance_pos = real_sem.index(x)
+            sem.append(real_sem[min_distance_pos])
         return sem
 
     # Get label of historical data
     def get_label_of_data(self):
         label = []
         for d in self.get_semantic_of_data():
-            label.append(self.words(self.get_real_semantics().index(d)))
+            label.append(self.words[self.get_real_semantics().index(d)])
         return label
 
     # Get linguistic logical relationship groups (repeat = True of False)
-    def get_rules(self, repeat, order):
-        if repeat:
-            pass
-        else:
-            pass
+    def get_rules(self):
+        labels = self.get_label_of_data()
+        for i in range(self.order, len(self.data)):
+            lhs = []
+            for j in range(i - self.order, i):
+                lhs.append(labels[j])
+            if lhs not in self.lhs:
+                self.lhs.append(lhs)
+                self.rhs.append([labels[i]])
+            else:
+                pos = self.lhs.index(lhs)
+                rhs = []
+                if type(self.rhs[pos]) != list:  # Check if current RHS is list or string
+                    rhs.append(self.rhs[pos])
+                else:
+                    for r in self.rhs[pos]:
+                        rhs.append(r)
+                if self.repeat:
+                    rhs.append(labels[i])
+                else:
+                    if (type(self.rhs[pos]) != str) and (labels[i] not in self.rhs[pos]):
+                        rhs.append(labels[i])
+                self.rhs[pos] = rhs
 
     # Get forecasted results (repeat = True of False)
-    def get_results(self, repeat, order):
-        if repeat:
-            pass
-        else:
-            pass
+    def get_results(self):
+        self.get_rules()
+        labels = self.get_label_of_data()
+        results = []
+        for i in range(self.order, len(self.data)):
+            lhs = []
+            for j in range(i - self.order, i):
+                lhs.append(labels[j])
+            for rule in self.lhs:
+                if lhs == rule:
+                    pos = self.lhs.index(rule)     # Position of rule will be used
+                    if type(self.rhs[pos]) != list:
+                        result = self.get_real_semantics()[self.words.index(self.rhs[pos])]
+                    else:
+                        total = 0
+                        count = 0
+                        for r in self.rhs[pos]:
+                            total += self.get_real_semantics()[self.words.index(r)]
+                            count += 1
+                        result = float(total / count)
+            results.append(result)
+        return results
--- main.py
+++ main.py
@@ -1,9 +1,54 @@
 # Main file of LTS procedure
+from Errors import Measure
+from LTS import LTS
+from HAs import HedgeAlgebras
 
+# Get dataset
 f = open('datasets/alabama.txt', 'r')
-
 data = list(map(int, f.readline().split(',')))
-lb = f.readline()
-ub = f.readline()
+lb = int(f.readline())
+ub = int(f.readline())
 
-print(data)
+# HA Model parameters
+theta = 0.46
+alpha = 0.52
+ha = HedgeAlgebras(theta, alpha)
+words = ha.get_words(3)
+# words = ["V-", "-", "L-", "W", "L+", "+", "V+"]
+
+# Time series forecasting model parameters
+order = 1
+repeat = False
+
+# Create forecasting model
+lts = LTS(order, repeat, data, lb, ub, words, theta, alpha)
+
+print("Words and their SQM:")
+print(words)
+print(lts.get_semantic())
+print(lts.get_real_semantics())
+
+print("Data labels:")
+print(lts.get_label_of_data())
+
+lts.get_rules()
+if repeat:
+    print("LLRGs (repeated):")
+else:
+    print("LLRGs (no-repeated):")
+for i in range(len(lts.lhs)):
+    print(lts.lhs[i], end='')
+    print(" => ", end='')
+    print(lts.rhs[i])
+
+print("Results:")
+forecasted = lts.get_results()
+print(forecasted)
+
+# Assessment
+for i in range(order):
+    data.pop(0)
+
+m = Measure(data, forecasted)
+print("MSE = " + str(m.mse()))
+print("RMSE = " + str(m.rmse()))
